34bf25524abac2563735b8b5db226fd33d06c4a7
Waldemar Kozaczuk <jwkozaczuk@gmail.com>
2016-08-22 01:03:57 +0000

    Added targets to build script to create profile specific rt.jar and resources.jar

--- build_openjdk_compact_profiles.sh
+++ build_openjdk_compact_profiles.sh
@@ -13,3 +13,13 @@ make -f GenerateVersionJava.gmk $WORKING_DIR/build/$PLATFORM_NAME/jdk/gen_profil
 #Compile 5 BUILD tool classes
 mkdir -p $WORKING_DIR/build/$PLATFORM_NAME/jdk/btclasses
 $WORKING_DIR/j2sdk-image/bin/javac -classpath $WORKING_DIR/j2sdk-image/lib/tools.jar -d $WORKING_DIR/build/$PLATFORM_NAME/jdk/btclasses $(find $WORKING_DIR/jdk/make/src/classes  -name "*.java")
+touch $WORKING_DIR/build/$PLATFORM_NAME/jdk/btclasses/_the.BUILD_TOOLS_batch
+
+#Build rt.jar 
+mkdir -p $WORKING_DIR/jdk/src/solaris/classes/sun/awt/X11/generator
+cd $WORKING_DIR/jdk/make
+make SPEC=$WORKING_DIR/build/$PLATFORM_NAME/spec.gmk PROFILE=profile_1 -I ../../make/common -f CreateJars.gmk $WORKING_DIR/build/$PLATFORM_NAME/images/libprofile_1/rt.jar
+
+#Build resources.jar
+make SPEC=$WORKING_DIR/build/$PLATFORM_NAME/spec.gmk PROFILE=profile_1 -I ../../make/common -f CreateJars.gmk $WORKING_DIR/build/$PLATFORM_NAME/images/libprofile_1/resources.jar
+
